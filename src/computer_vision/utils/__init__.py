660c61b33206606c124a9114c1b7f55707a7ebb5
Sy Viet <sy.dao@cec.valantic.com>
2024-08-22 21:23:59 +0200

    Add path manager for handling image and video paths

---
+++ src/computer_vision/utils/__init__.py
@@ -0,0 +1,1 @@
+from .path_manager import get_video, get_image
---
+++ src/computer_vision/utils/path_manager.py
@@ -0,0 +1,17 @@
+import os
+
+
+def get_base_image_path(state='raw'):
+    return os.path.join(os.path.dirname(__file__), '..', '..', '..', 'data', state, 'images')
+
+
+def get_image(filename: str, state: str = None):
+    return os.path.join(get_base_image_path(state), filename)
+
+
+def get_base_video_path(state='raw'):
+    return os.path.join(os.path.dirname(__file__), '..', '..', '..', 'data', state, 'input_videos')
+
+
+def get_video(filename: str, state: str = None):
+    return os.path.join(get_base_video_path(state), filename)
