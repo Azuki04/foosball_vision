eb36d89d24df347fbfb4e1dbb43fbd8e294db19f
Sy Viet <sy.dao@cec.valantic.com>
2024-08-22 21:31:44 +0200

    Fix path manager

--- src/computer_vision/utils/path_manager.py
+++ src/computer_vision/utils/path_manager.py
@@ -5,7 +5,7 @@ def get_base_image_path(state='raw'):
     return os.path.join(os.path.dirname(__file__), '..', '..', '..', 'data', state, 'images')
 
 
-def get_image(filename: str, state: str = None):
+def get_image(filename: str, state: str = 'raw'):
     return os.path.join(get_base_image_path(state), filename)
 
 
@@ -13,5 +13,5 @@ def get_base_video_path(state='raw'):
     return os.path.join(os.path.dirname(__file__), '..', '..', '..', 'data', state, 'input_videos')
 
 
-def get_video(filename: str, state: str = None):
+def get_video(filename: str, state: str = 'raw'):
     return os.path.join(get_base_video_path(state), filename)
