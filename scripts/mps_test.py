14861c1fc90f3eb9fce7aff1a13835ac502cd684
Sy Viet <sy.dao@cec.valantic.com>
2024-10-13 17:51:49 +0200

    change script

--- scripts/mps_test.py
+++ scripts/mps_test.py
@@ -1,18 +1,16 @@
 from ultralytics import YOLO
 import torch
 
+
 def main():
-
     print(torch.backends.mps.is_available())
-
-    model = YOLO('../data/processed/model/yolo11n-best.pt')
+    # yolov5su, yolov8x, yolov10x, yolov10s, yolov11n, yolov11s, yolov11m, yolov11l, yolov11x
+    model = YOLO('../weights/yolo11n-best.pt')
 
     source = 0
-    #device="mps"
+    # device="mps"
     model.predict(source=source, imgsz=640, save=False, show=True)
+
 
 if __name__ == '__main__':
     main()
-
-
-
