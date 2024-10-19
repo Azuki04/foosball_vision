92367ff5f4af8fc03ea202a8ba97ef5337ce3699
Sy Viet <sy.dao@cec.valantic.com>
2024-10-19 22:15:15 +0200

    Fix motor control

--- src/motor_controllor/src/setup.cpp
+++ src/motor_controllor/src/setup.cpp
@@ -4,26 +4,39 @@
 #include <Arduino.h>
 
 //------------------------------------------------------------------------------
-// KONSTANTEN
+// CONSTANTS (MOTOR PINS)
 //------------------------------------------------------------------------------
+    
+// Configuration for Bar 1 (M1 - M4)
 
-    #define M1_STEP 2
-    #define M1_DIR  5
+// Rotation (M4)
+#define M4_STEP 5
+#define M4_DIR  4
 
-    #define M2_STEP 4
-    #define M2_DIR  7
+// Linear (M1)
+#define M1_STEP 3
+#define M1_DIR  2
 
-    #define M3_STEP 9
-    #define M3_DIR  8
+// Configuration for Bar 2 (M5 - M2)
 
-    #define M4_STEP 11
-    #define M4_DIR  10
+// Rotation (M5)
+#define M5_STEP 9
+#define M5_DIR  8
 
-    #define M5_STEP 3
-    #define M5_DIR  6
+// Linear (M2)
+#define M2_STEP 7
+#define M2_DIR  6
 
-    #define M6_STEP 12 
-    #define M6_DIR  13
+// Configuration for Bar 3 (M6 - M3)
+
+// Rotation (M6)
+#define M6_STEP 13
+#define M6_DIR  12
+
+// Linear (M3)
+#define M3_STEP 11
+#define M3_DIR  10
+ 
 
 //------------------------------------------------------------------------------
 // Method Declaration for Motor Control
@@ -32,62 +45,67 @@
 void m1step(int dir) {
     digitalWrite(M1_DIR, dir == 1 ? HIGH : LOW);
     digitalWrite(M1_STEP, HIGH);
-    delayMicroseconds(1);
+    delayMicroseconds(500);
     digitalWrite(M1_STEP, LOW);
-    delayMicroseconds(1);
+    delayMicroseconds(500);
 }
 
 void m2step(int dir) {
     digitalWrite(M2_DIR, dir == 1 ? HIGH : LOW);
     digitalWrite(M2_STEP, HIGH);
-    delayMicroseconds(1);
+    delayMicroseconds(500);
     digitalWrite(M2_STEP, LOW);
-    delayMicroseconds(1);
+    delayMicroseconds(500);
 }
 
 void m3step(int dir) {
     digitalWrite(M3_DIR, dir == 1 ? HIGH : LOW);
     digitalWrite(M3_STEP, HIGH);
-    delayMicroseconds(1);
+    delayMicroseconds(500);
     digitalWrite(M3_STEP, LOW);
-    delayMicroseconds(1);
+    delayMicroseconds(500);
 }
 
 void m4step(int dir) {
     digitalWrite(M4_DIR, dir == 1 ? HIGH : LOW);
     digitalWrite(M4_STEP, HIGH);
-    delayMicroseconds(1);
+    delayMicroseconds(500);
     digitalWrite(M4_STEP, LOW);
-    delayMicroseconds(1);
+    delayMicroseconds(500);
 }
 
 void m5step(int dir) {
     digitalWrite(M5_DIR, dir == 1 ? HIGH : LOW);
     digitalWrite(M5_STEP, HIGH);
-    delayMicroseconds(1);
+    delayMicroseconds(500);
     digitalWrite(M5_STEP, LOW);
-    delayMicroseconds(1);
+    delayMicroseconds(500);
 }
 
 void m6step(int dir) {
     digitalWrite(M6_DIR, dir == 1 ? HIGH : LOW);
     digitalWrite(M6_STEP, HIGH);
-    delayMicroseconds(1);
+    delayMicroseconds(500);
     digitalWrite(M6_STEP, LOW);
-    delayMicroseconds(1);
+    delayMicroseconds(500);
 }
 
 void setup_controller() {
     pinMode(M1_STEP, OUTPUT);
     pinMode(M1_DIR, OUTPUT);
+
     pinMode(M2_STEP, OUTPUT);
     pinMode(M2_DIR, OUTPUT);
+
     pinMode(M3_STEP, OUTPUT);
     pinMode(M3_DIR, OUTPUT);
+
     pinMode(M4_STEP, OUTPUT);
     pinMode(M4_DIR, OUTPUT);
+
     pinMode(M5_STEP, OUTPUT);
     pinMode(M5_DIR, OUTPUT);
+
     pinMode(M6_STEP, OUTPUT);
     pinMode(M6_DIR, OUTPUT);
 }
