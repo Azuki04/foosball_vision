d2dd74b74fd2acde5afff8830409fa80489c70e1
Sy Viet <sy.dao@cec.valantic.com>
2024-10-20 11:18:09 +0200

    Add serial sender class

--- src/computer_vision/utils/__init__.py
+++ src/computer_vision/utils/__init__.py
@@ -1,3 +1,4 @@
 from .path_manager import get_video, get_image, get_config, get_weight
 from .camera_setup import CameraSetup
 from .court_position_mapper import CourtPositionMapper
+from .serial_sender import SerialSender
---
+++ src/computer_vision/utils/serial_sender.py
@@ -0,0 +1,35 @@
+import time
+
+import serial
+
+from computer_vision.constants import (COURT_HEIGHT, COURT_WIDTH)
+
+
+class SerialSender:
+    def __init__(self, url, baudrate):
+        try:
+            self.ser = serial.serial_for_url(url, baudrate=baudrate, timeout=1)
+            time.sleep(2)
+            print("Serial port connected.")
+        except serial.SerialException as e:
+            print(f"Could not open serial port: {e}")
+            self.ser = None
+
+    def send_current_coordinate(self, x, y):
+        if not (0 <= x <= COURT_HEIGHT) or not (0 <= y <= COURT_WIDTH):
+            print("Coordinates are out of range")
+            return
+
+        try:
+            data = f"{x},{y}\n"
+            self.ser.write(data.encode('utf-8'))
+            print("Data has been sent")
+        except serial.SerialTimeoutException:
+            print("Timeout occurred while sending data.")
+        except serial.SerialException as e:
+            print(f"Error sending data: {e}")
+
+    def close(self):
+        if self.ser:
+            self.ser.close()
+            print("Serial port closed.")
